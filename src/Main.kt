3247625c3d86b86e99a36feb6b1be73eaa32ddd6
Ярослав Нестеров <atomofiron@yandex.ru>
2017-10-21 14:35:03 +0300

    Main class was added

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner
+
+object Main {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val scan = Scanner(System.`in`)
+
+        val str = scan.next()
+
+        println("kek: $str")
+    }
+}
